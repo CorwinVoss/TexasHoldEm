1b62e03b9f10af650b5cd78ca4363fa46db73baa
Corwin Voss <cvoss@students.inghamisd.org>
2018-03-22 13:25:26 -0400

    Added Classes

---
+++ src/com/company/Deck.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by cvoss on 3/22/2018.
+ */
+public class Deck {
+}
---
+++ src/com/company/Entity.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.ArrayList;
+
+/**
+ * Created by cvoss on 3/22/2018.
+ */
+public class Entity {
+    int Currency;
+    ArrayList<Card> hand = new
+    public Entity(){
+
+    }
+}
