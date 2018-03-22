3415db55114c30313be7b67600be0f11ce81f4e4
Corwin Voss <cvoss@students.inghamisd.org>
2018-03-22 13:56:12 -0400

    Added functions to Entity

--- src/com/company/Entity.java
+++ src/com/company/Entity.java
@@ -1,14 +1,28 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by cvoss on 3/22/2018.
  */
 public class Entity {
-    int Currency;
-    ArrayList<Card> hand = new
+    protected int currency;
+    ArrayList<Card> hand;
+    Random rand;
+    int betPool;
     public Entity(){
+    hand = new ArrayList<>();
+    rand = new Random();
+    }
 
+    protected void Bet(int currency, int bet) {
+        this.currency = currency;
+        this.currency -= bet;
+        this.betPool += bet;
+    }
+
+    protected void Win(int betPool){
+        this.currency += betPool;
     }
 }
